71d16501cb3a6c9dd9fea4dc602b4afbbe25c3a6
mstaud <mstaud@944698e4-1508-0410-9358-9c0c139b3dd7>
2006-03-21 20:11:26 +0000

    Parser

---
+++ com.xiaobo.uml/src/uti/parser/LinkMemory.java
@@ -0,0 +1,11 @@
+package uti.parser;
+
+import uti.codeModel.BaseCode;
+import uti.java.Link;
+
+public class LinkMemory {
+   static void add(Link l, BaseCode code, LinkName ln, Class expected)
+   {
+	   
+   }
+}
---
+++ com.xiaobo.uml/src/uti/parser/LinkName.java
@@ -0,0 +1,22 @@
+package uti.parser;
+
+import java.util.*;
+
+public class LinkName {
+   Vector names=new Vector();
+   Vector tokens = new Vector();
+   void addValue(String name, Token t)
+   {
+	   names.addElement(names);
+	   tokens.addElement(t);
+	   
+   }
+   int getCount()
+   {
+	   return names.size();
+   }
+   String getValue(int i)
+   {
+	   return (String)names.elementAt(i);
+   }
+}
